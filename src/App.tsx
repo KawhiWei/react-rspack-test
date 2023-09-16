28fcb74875372eabf58e12295bc67f7df0c383fc
kawhi <1585955375@qq.com>
2023-09-16 10:32:45 +0800

    feat ： 修改名字

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
+
+import reactLogo from "./assets/react.svg";
+import { useState } from "react";
 
 function App() {
 	const [count, setCount] = useState(0);
@@ -12,7 +13,7 @@ function App() {
 					<img src={reactLogo} className="logo react" alt="React logo" />
 				</a>
 			</div>
-			<h1>Rspack + React + TypeScript</h1>
+			<h1>Toyar-PaaS</h1>
 			<div className="card">
 				<button onClick={() => setCount(count => count + 1)}>
 					count is {count}
