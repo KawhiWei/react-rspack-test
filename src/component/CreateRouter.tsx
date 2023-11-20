0f4902851ef96282407a5f882de2ecbf4d4ef643
kawhi <1585955375@qq.com>
2023-11-20 14:47:10 +0800

    feature 1、处理路由

--- src/component/CreateRouter.tsx
+++ src/component/CreateRouter.tsx
@@ -1,36 +1,11 @@
-import { IMenuOutput, IMenuRoute } from '@/shared/menu/IMenu';
-import { Route, useNavigate } from 'react-router-dom'
+import { Route, useNavigate } from 'react-router-dom';
 
-import { Guid } from "guid-typescript";
-import HomePage from "@/pages/home-page/home-page";
-import LayoutView from "@/layout/layout-view";
 import { USER_MENU } from '@/store/actionType';
 import { message } from 'antd';
 import store from '@/store';
 import { useEffect } from 'react';
 
-const Main: IMenuRoute[] = [
-    {
-        id: Guid.EMPTY,
-        name: "layout",
-        path: "/",
-        component: LayoutView,
-        isShow: false,
-        children: [
-            {
-                id: Guid.create().toString(),
-                name: "主页",
-                path: "/home",
-                component: HomePage,
-                isShow: true,
-                exact: true,
-                children: [],
-            }
-        ]
-    }
-]
-
-const CreateRouter = (props: any) => {
+export const CreateRouter = (props: any) => {
 
     const token = localStorage.getItem("token");
     const { config, location } = props;
@@ -42,22 +17,21 @@ const CreateRouter = (props: any) => {
             // let res = await this.menus.getMenus();
             // if (res.success) {
             // this.menus.setMenus(res.data);
-            store.dispatch({
+            await store.dispatch({
                 type: USER_MENU,
                 // data: this.menus.menusByShow
                 data: require("@/constans/menu").menuList
-            })
+            });
             // message.success(res.message);
             // } else {
             //   message.error(res.message);
             // }
-
         } catch (error: any) {
             message.error(error);
         }
-    }
+    };
 
-    Route
+    Route;
     /**
      * 页面初始化事件
      */
@@ -80,11 +54,11 @@ const CreateRouter = (props: any) => {
 
     /**
      * 是否登录节点或者跟节点
-     * @param _pathname 
+     * @param _pathname
      */
     const isNotLoginOrRootNode = (_pathname: string): boolean => {
-        return (pathname === "/login" || pathname === "/")
-    }
+        return (pathname === "/login" || pathname === "/");
+    };
 
 
 
@@ -92,8 +66,8 @@ const CreateRouter = (props: any) => {
      * 是否仅有一个节点
      */
     const isOneChildren = (): boolean => {
-        return Main[0].children.length === 1
-    }
+        return Main[0].children.length === 1;
+    };
 
 
     return (
@@ -102,31 +76,5 @@ const CreateRouter = (props: any) => {
 
         </div>
 
-    )
-}
-
-export default CreateRouter;
-
-// import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-// import Page1 from './pages/page1'
-// import Page2 from './pages/page2'
-// import Page3 from './pages/page3'
-
-// const router = createBrowserRouter([{
-//   path: '/page1',
-//   Component: Page1,
-// }, {
-//   path: '/page2',
-//   Component: Page2,
-// }, {
-//   path: '/page3',
-//   Component: Page3,
-// }])
-
-// function App() {
-//   return (
-//     <RouterProvider router={router} />
-//   )
-// }
-
-// export default App
+    );
+};
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes } from "react-router-dom";
 import React, { Suspense } from "react";
 
+import CreateRouter from "@/component/createRouterMenu";
 import Login from "@/router/constans/login";
 import NotFound from "@/router/constans/notFound";
 import RouterAuth from "@/component/routeAuth";
@@ -16,7 +17,8 @@ class BasicRoute extends React.Component {
       <BrowserRouter>
         <Routes>
           <Suspense fallback={<div>Loading...</div>}>
-            <RouterAuth config={routes} />
+            {/* <RouterAuth config={routes} /> */}
+            <CreateRouter config={routes} />
           </Suspense>
         </Routes>
       </BrowserRouter>
